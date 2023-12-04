4fcd03521d466f74b9b5e65fe611bae306975a39
garyb9 <gberkinfand@gmail.com>
2023-12-04 19:37:55 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-
 #![allow(dead_code)]
 #![allow(unused_variables)]
 mod setup;
 
 fn main() {
     // Prover gets the setup
-    let for_prover: (Vec<bls12_381::G1Projective>, Vec<bls12_381::Scalar>) = setup::setup_for_prover();
+    let for_prover: (Vec<bls12_381::G1Projective>, Vec<bls12_381::Scalar>) =
+        setup::setup_for_prover();
     let lagrange_basis: Vec<bls12_381::G1Projective> = for_prover.0;
     let polynomial: Vec<bls12_381::Scalar> = for_prover.1;
 
@@ -20,6 +20,5 @@ fn main() {
     //     println!("Proof verified: Prover knows the secret.");
     // } else {
     //     println!("Proof not verified: Prover does not know the secret.");
-    // }    
-    
+    // }
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,14 +1,14 @@
-// mod consts;
-use bls12_381::{Scalar, G1Affine, G1Projective};
-use rand::thread_rng;
+use bls12_381::{G1Affine, G1Projective, Scalar};
 use ff::Field;
+use rand::thread_rng;
 
 pub const N: u64 = 1 << 17; // 2**17
 
-pub fn setup_for_prover() -> (Vec<G1Projective> , Vec<Scalar>) {
+pub fn setup_for_prover() -> (Vec<G1Projective>, Vec<Scalar>) {
     let tau_group: Vec<bls12_381::Scalar> = generate_random_tau_group();
     let g_srs: Vec<bls12_381::G1Projective> = generate_srs(&tau_group);
-    let lagrange_basis: Vec<bls12_381::G1Projective> = convert_to_lagrange_basis(&tau_group, &g_srs);
+    let lagrange_basis: Vec<bls12_381::G1Projective> =
+        convert_to_lagrange_basis(&tau_group, &g_srs);
     let polynomial: Vec<bls12_381::Scalar> = generate_random_polynomial();
     (lagrange_basis, polynomial)
 }
@@ -17,18 +17,23 @@ pub fn generate_random_polynomial() -> Vec<Scalar> {
     let mut polynomial: Vec<Scalar> = Vec::new();
 
     // Iterate from 0 to 2*N - 1
-    let size: u64 = 2*N-1;
+    let size: u64 = 2 * N - 1;
     for i in 0..(2 * N) {
         let curr: Scalar = generate_random_scalar();
         polynomial.push(curr);
-        println!("{:?}/{:?}: Adding (C_{:?})={:?} coefficient to polynomical in Fr", i, size, i, curr);
+        println!(
+            "{:?}/{:?}: Adding (C_{:?})={:?} coefficient to polynomial in Fr",
+            i, size, i, curr
+        );
     }
     println!("Done generating Polynomial");
     polynomial
-
 }
 
-pub fn convert_to_lagrange_basis(tau_group: &Vec<Scalar>, g_srs: &Vec<G1Projective>) -> Vec<G1Projective> {
+pub fn convert_to_lagrange_basis(
+    tau_group: &Vec<Scalar>,
+    g_srs: &Vec<G1Projective>,
+) -> Vec<G1Projective> {
     let mut lagrange_basis: Vec<G1Projective> = Vec::new();
 
     for (i, &x) in tau_group.iter().enumerate() {
@@ -45,7 +50,10 @@ pub fn convert_to_lagrange_basis(tau_group: &Vec<Scalar>, g_srs: &Vec<G1Projecti
 
         let lagrange_coefficient: G1Projective = numerator * denominator.invert().unwrap();
         lagrange_basis.push(lagrange_coefficient);
-        println!("Lagrange Coefficient {:?} computed as: {:?}", i, lagrange_coefficient);
+        println!(
+            "Lagrange Coefficient {:?} computed as: {:?}",
+            i, lagrange_coefficient
+        );
     }
 
     lagrange_basis
@@ -57,11 +65,14 @@ pub fn generate_srs(tau_group: &Vec<Scalar>) -> Vec<G1Projective> {
     let mut g_srs: Vec<G1Projective> = Vec::new();
 
     // Iterate from 0 to 2*N - 1
-    let size: u64 = 2*N-1;
+    let size: u64 = 2 * N - 1;
     for (i, &scalar) in tau_group.iter().enumerate() {
         let curr: G1Projective = random_group_element * scalar;
         g_srs.push(curr);
-        println!("{:?}/{:?}: Adding (G_{:?})={:?} to [G]_SRS vector", i, size, i, curr);
+        println!(
+            "{:?}/{:?}: Adding (G_{:?})={:?} to [G]_SRS vector",
+            i, size, i, curr
+        );
     }
     println!("Done generating [G]_SRS");
     g_srs
@@ -72,16 +83,18 @@ pub fn generate_random_tau_group() -> Vec<Scalar> {
     let mut tau_group: Vec<Scalar> = Vec::new();
 
     // Iterate from 0 to 2*N - 1
-    let size = 2*N-1;
+    let size = 2 * N - 1;
     for i in 0..(2 * N) {
         let curr = random_tau.pow(&[i, 0, 0, 0]);
         tau_group.push(curr);
-        println!("{:?}/{:?}: Adding (tau^{:?})={:?} to tau group vector", i, size, i, curr);
+        println!(
+            "{:?}/{:?}: Adding (tau^{:?})={:?} to tau group vector",
+            i, size, i, curr
+        );
     }
     println!("Done generating Tau Group");
     tau_group
 }
-
 
 pub fn generate_random_scalar() -> Scalar {
     let mut rng = thread_rng();
